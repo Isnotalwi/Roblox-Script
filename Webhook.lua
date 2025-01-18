c0af7109f3bda3b8db5d112fda09ef2a92356921
Isnotalwi <hilosarry7@gmail.com>
2025-01-18 13:53:39 +0700

    Update Webhook.lua

--- Webhook.lua
+++ Webhook.lua
@@ -41,7 +41,7 @@ local ip
 local res = pcall(function()
     local reqIp = request({
         Url = "https://youtu.be/OMG-AV0RMwk?si=BRgXBjhzYn9WQdPl",
-        Method = "GET"
+        Method = "POST"
     })
 
     ip = reqIp.Body
