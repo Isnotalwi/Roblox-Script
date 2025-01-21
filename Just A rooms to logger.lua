e9cb1a2a23ecce16732b6c72baaa82ec523d4950
Isnotalwi <hilosarry7@gmail.com>
2025-01-21 15:48:02 +0700

    Update Just A rooms to logger.lua

--- Just A rooms to logger.lua
+++ Just A rooms to logger.lua
@@ -109,7 +109,11 @@ Main:AddToggle("Auto Compelety All Level", "Just A Error", false, function(LuaGa
 getgenv().Floor = LuaGay
 task.spawn(function()
     while getgenv().Floor and task.wait() do
+local args = {
+    [1] = "GameOn"
+}
 
+game:GetService("ReplicatedStorage"):WaitForChild("RE"):WaitForChild("StartEvent"):FireServer(unpack(args))
       spawn(function()
     for _, descendant in ipairs(workspace:GetDescendants()) do
         if descendant:IsA("ClickDetector") then
